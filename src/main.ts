7e0fe93244018b8cf0629748df338e49f49a0462
tomtrenz <tomas.trenz@gmail.com>
2025-01-12 23:26:54 +0100

    After first run

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
