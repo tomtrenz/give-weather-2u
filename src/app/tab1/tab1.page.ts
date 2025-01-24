74e16f5bc2bcb588170d0c42a89aec3e0b6cf8cc
tomtrenz <tomas.trenz@gmail.com>
2025-01-24 01:19:18 +0100

    First weather fetch

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Location, WeatherMessage, WeatherService } from "../api/weather.service";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
   standalone: false,
 })
 export class Tab1Page {
+  weatherMsg?: WeatherMessage;
+  lang: string = "cs";
+  lokace: string = "Brno";
 
-  constructor() {}
 
+  constructor(private weatherService: WeatherService) {}
+
+  async searchClicked() {
+    console.log(this.weatherMsg?.location);
+
+    this.weatherMsg = await this.weatherService.fetchCurrentInfo(
+      this.lokace, this.lang
+    );
+  }
 }
